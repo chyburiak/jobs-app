4775c1472c1de9571f3b0fe7e4e14a563ad2c414
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2024-10-23 00:14:33 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppHeader from '@/components/AppHeader.vue';
+import HomeView from '@/views/HomeView.vue'
+import AppFooter from '@/components/AppFooter.vue';
+
+const apiKey = import.meta.env.VITE_API_KEY;
+</script>
+
+<template>
+  <main class="w-full min-h-screen flex flex-col gap-2.5 font-primary">
+    <AppHeader />
+
+    <HomeView />
+
+    <AppFooter />
+  </main>
+</template>
